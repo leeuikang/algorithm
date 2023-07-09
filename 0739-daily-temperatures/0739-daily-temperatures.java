63e12a9340e964639f5ee16c4ae25f9ecadb93c4
leeuikang <56907030+leeuikang@users.noreply.github.com>
2023-07-09 16:05:35 +0900

    Time: 176 ms (37.68%), Space: 58.3 MB (23.09%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        
+        int[] answer = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i = answer.length - 1;  i >= 0; i--){
+            
+            while(!stack.isEmpty()){
+                if(temperatures[i] < temperatures[stack.peek()]){
+                    answer[i] = stack.peek() - i;
+                    break;
+                }
+            
+                stack.pop();
+            }
+            stack.push(i);
+        }
+    
+        
+        return answer;
+    }
+}
